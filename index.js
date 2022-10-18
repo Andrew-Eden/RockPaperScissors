43ec333e7c47a68c12ac12da104bf601dcf47eee
Andrew-Eden <eden9210@gmail.com>
2022-10-18 00:32:51 -0400

    2nd

---
+++ index.js
@@ -0,0 +1,33 @@
+function getComputerChoice() {
+    let computerSelectionRandomizer = Math.floor(Math.random() * 3) + 1;
+    switch (computerSelectionRandomizer) {
+        case 1:
+            computerSelection = "rock";
+            break;
+        case 2:
+            computerSelection = "paper";
+            break;
+        case 3:
+            computerSelection = "scissors";
+    }
+    console.log(computerSelection);
+    return computerSelection;
+}
+
+function getPlayerChoice() {
+    let playerSelection = prompt("What's your choice?").toLowerCase();
+    return playerSelection;
+}
+
+function playRound(playerSelection, computerSelection) {
+    switch (playerSelection) {
+        case "rock":
+
+        case "paper":
+
+        case "scissors":
+
+        default:
+            return "error";
+    }
+}
